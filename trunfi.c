bd1b18c67ee06ee0a2f67166be33910838c20a82
leonart8 <ls6822392@gmail.com>
2025-05-03 17:41:42 -0300

    Update trunfi.c

--- trunfi.c
+++ trunfi.c
@@ -2,20 +2,11 @@
 
 int main(){
 //declarando váriaveis.
-    char estado[50];
-    char estado2[50];
-    char codest[5];
-    char codest2[5];
-    char cidade[50];
-    char cidade2[50];
+    char estado[50], estado2[50], codest[6], codest2[6],  cidade[50], cidade2[50];
     int populacao , desidade;
     int populacao2, desidade2;
-    int npt, deci;
-    int npt2;
-    float area;
-    float area2;
-    float pib;
-    float pib2;
+    int npt, deci, npt2;
+    float area, area2, pib, pib2;;
   
     printf("digite 1 se quer que o computador gere a carta ou qualquer 2 se gostaria que o segundo jogador digitasse \n");
     scanf("%i" , &deci);
@@ -29,7 +20,7 @@ int main(){
 
        fgets(cidade,50,stdin);
     printf("Agora digite o código da cidade com a letra do estado seguida de um número de 01 a 04 (ex: A01, B03).:\n");
-       fgets(codest,5,stdin);
+       fgets(codest,6,stdin);
     printf("Digite o total populacional:\n");
       scanf(" %i", &populacao);
     printf("Digite a área da cidade:\n");
@@ -47,8 +38,11 @@ int main(){
           double area2 = 1521;
           int pib2 =  3;
           int npt2 = 5;
+          //calculando densidades das cartas
          double desidade2 = populacao2/area2;
         double desidade = populacao/area;
+
+        //utilizando if e else para exibir o resultado
          if (desidade2 > desidade)
          {
           printf("\n Carta 01: %s Código: (%s) desidade populacional: %d \n",cidade,codest, desidade);
@@ -61,10 +55,6 @@ int main(){
           printf("\n Carta 02: %s Código:  (%s) desidade populacional: %d",cidade2,codest2, desidade2);  
           printf("Carta 01: %s (%s) ganhou!", cidade,codest); 
          }
-         
-        
-
-        
           return 0;
 
     }
@@ -77,7 +67,7 @@ else
     printf("Digite nome da cidade:\n");
        fgets(cidade,50,stdin);
     printf("Agora digite o código da cidade com a letra do estado seguida de um número de 01 a 04 (ex: A01, B03).:\n");
-       fgets(codest,5,stdin);
+       fgets(codest,6,stdin);
     printf("Digite o total populacional:\n");
       scanf(" %i", &populacao);
     printf("Digite a área da cidade:\n");
@@ -87,7 +77,6 @@ else
     printf("Digite o número de pontos turisticos que tem nessa cidade:\n");
       scanf(" %i", &npt);
     
-  
      // recebendo dados da carta 02
   printf("\ncarta 02\nDigite o estado representado por uma letra de 'A' até 'H':\n");
   getchar();
@@ -119,9 +108,8 @@ else
     printf("carta 1: %s , %s :%f ");
   }
   
-  
-     
     return 0;
-}
- return 0;
+
+  }
+  return 0;
 }
